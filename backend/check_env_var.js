0a5cc3d9c9a74176250da983fed476d3572e5ebf
deepdas2607 <157793903+deepdas2607@users.noreply.github.com>
2025-12-31 21:26:33 +0530

    Delete backend directory

--- backend/check_env_var.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-console.log('Checking environment variables...');
-if (process.env.GEMINI_API_KEY) {
-    console.log('GEMINI_API_KEY is set (Length: ' + process.env.GEMINI_API_KEY.length + ')');
-} else {
-    console.log('GEMINI_API_KEY is MISSING');
-}
